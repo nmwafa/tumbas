dbe08e74b9dabe316e090de27bf10727163e0bb4
Nur Muhammad Wafa <wafamuh529@gmail.com>
2026-09-10 14:32:42 +0700

    change logo

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
 app.use(session({
   secret: 'purworejo-secret-key-1945',
   resave: false,
